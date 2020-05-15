c0ddaea148a2106d1cd5019da18750f162fc6e3c
takahashim <maki@rubycolor.org>
2020-05-15 21:26:04 +0900

    git init

---
+++ target/macosx_xcode/target_syssvc.h
@@ -0,0 +1,19 @@
+/*
+ *		システムサービスのターゲット依存部（Mac OS X用）
+ *
+ *  システムサービスのターゲット依存部のヘッダファイル．システムサービ
+ *  スのターゲット依存の設定は，できる限りコンポーネント記述ファイルで
+ *  記述し，このファイルに記述するものは最小限とする．
+ * 
+ *  $Id: target_syssvc.h 1051 2018-11-19 14:00:52Z ertl-hiro $
+ */
+
+#ifndef TOPPERS_TARGET_SYSSVC_H
+#define TOPPERS_TARGET_SYSSVC_H
+
+/*
+ *  ターゲットシステムのOS依存の定義
+ */
+#include "macosx.h"
+
+#endif /* TOPPERS_TARGET_SYSSVC_H */
